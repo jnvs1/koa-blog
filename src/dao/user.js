7a527aa05e2e7c93fd5290a7f4b5af1e464374a8
jnvs1 <luxieque21936899@126.com>
2021-04-21 10:38:00 +0800

    update

--- src/dao/user.js
+++ src/dao/user.js
@@ -1,5 +1,20 @@
 const { query } = require('../db/db')
 
+const createUser = (values) => {
+  const sql = 'insert into user (username, password, age) values (?, ?, ?)'
+  return query(sql, values)
+}
+
+const deleteUser = (values) => {
+  const sql = 'delete from user where username = ?'
+  return query(sql, values)
+}
+
+const getUsers = (values) => {
+  const sql = 'select id, username, password, age from user where username = ?'
+  return query(sql, values)
+}
+
 const getAllUsers = (values) => {
   const sql = 'select * from user'
   return query(sql, values)
@@ -11,6 +26,9 @@ const getUserById = (values) => {
 }
 
 module.exports = {
+  createUser,
+  getUsers,
+  deleteUser,
   getAllUsers,
-  getUserById
+  getUserById,
 }
